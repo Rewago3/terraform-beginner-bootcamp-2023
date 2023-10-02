b5084dcf2d21bdb849f33810538d748c7ec78f50
Rewago3 <123596308+Rewago3@users.noreply.github.com>
2023-10-01 22:49:21 -0600

    23 configuration drift (#25)
    * #23 Bucket should no longer use random
    
    * 23 commiting left out files

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 
-resource "random_string" "bucket_name" {
-  lower = true
-  upper = false
-  length           = 32
-  special          = false
-}
-resource "aws_s3_bucket" "example" {
-  bucket = random_string.bucket_name.result
+# resource "random_string" "bucket_name" {
+#   lower = true
+#   upper = false
+#   length           = 32
+#   special          = false
+# }
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
 
   tags = {
    UserUuid = var.user_uuid
